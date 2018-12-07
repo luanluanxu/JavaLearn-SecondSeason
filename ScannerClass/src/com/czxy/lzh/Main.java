430c05008391d3aa94a0fa185d2847c13412b002
luanluanxu <100981384@qq.com>
2018-12-07 11:49:55 +0800

    ScannerClass

---
+++ ScannerClass/src/com/czxy/lzh/Main.java
@@ -0,0 +1,14 @@
+package com.czxy.lzh;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str;
+        while (!s.hasNext("q")) {
+            System.out.println(s.next());
+        }
+    }
+}
